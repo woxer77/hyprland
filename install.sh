204a3466fe2026f8e73cf83c7790b2cb7867b421
Denis Ohrimenko <woxer77@gmail.com>
2025-06-24 22:29:43 +0300

    added missed packages and enhanced install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -ex  # Остановиться при ошибке и печатать ко
 
 USERNAME="woxer"
 USER_HOME="/home/$USERNAME"
-REPO_DIR="$(pwd)"  # Предполагаем, что скрипт запускается из корня репозитория
+REPO_DIR="$(pwd)/hyprland"  # Предполагаем, что скрипт запускается из корня репозитория
+WG_SCRIPT="$USER_HOME/scripts/wireguard-install.sh"
 
 # Проверка и создание пользователя
 if ! id "$USERNAME" &>/dev/null; then
@@ -49,6 +50,12 @@ else
   echo "[-] File packages/yay.txt not found!"
 fi
 
+# Downloading fonts from Google Fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/google/fonts/raw/main/ofl/rubikwetpaint/RubikWetPaint-Regular.ttf
+
+
 # Копирование конфигов
 echo "[+] Copying configs..."
 if [ -d "$REPO_DIR/configs/.config" ]; then
@@ -62,12 +69,15 @@ fi
 # Права
 chown -R "$USERNAME:$USERNAME" "$USER_HOME/.config" "$USER_HOME/.bashrc" 2>/dev/null || true
 
+mkdir -p ~/Downloads
+
 # Копирование скриптов и обоев
 echo "[+] Copying scripts and wallpapers..."
 if [ -d "$REPO_DIR/scripts" ]; then
-  cp -r "$REPO_DIR/scripts" "$USER_HOME/scripts"
+  mv "$REPO_DIR/scripts/volume-down.sh" /usr/local/bin/
+  mv "$REPO_DIR/scripts/volume-up.sh" /usr/local/bin/
+  mv "$REPO_DIR/scripts/unzip_to_folder" /usr/local/bin/
 fi
-
 if [ -d "$REPO_DIR/Wallpapers" ]; then
   cp -r "$REPO_DIR/Wallpapers" "$USER_HOME/Wallpapers"
 fi
@@ -81,5 +91,13 @@ echo "[+] Configuring sudoers for wg-quick..."
   echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/wg-quick up /home/$USERNAME/.config/wireguard/wg-client.conf"
   echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/wg-quick down /home/$USERNAME/.config/wireguard/wg-client.conf"
 } >> /etc/sudoers
+
+# Инициализируем wireguard
+if [[ -f "$WG_SCRIPT" ]]; then
+    echo "Launching WireGuard installation..."
+    bash "$WG_SCRIPT"
+else
+    echo "Error: Script $WG_SCRIPT wasn't found!"
+fi
 
 echo "[✓] Hyprland environment installed and configured successfully!"
